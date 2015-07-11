24f28c9a5fa70366f9773da78dbf4f82018fc82c
James Rosewell <james@51degrees.com>
2015-07-11 15:05:26 +0100

    Implemented IWrapper to make Pattern and Trie identical to consumer

---
+++ VisualStudio/Interop/IWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace FiftyOne.Mobile.Detection.Provider.Interop
+{
+    public interface IWrapper : IDisposable
+    {
+        /// <summary>
+        /// Returns a list of properties and values for the user agent
+        /// provided.
+        /// </summary>
+        /// <param name="userAgent"></param>
+        /// <returns></returns>
+        SortedList<string, List<string>> GetProperties(string userAgent);
+
+        /// <summary>
+        /// Returns a list of properties and values for the HTTP headers
+        /// provided.
+        /// </summary>
+        /// <param name="headers"></param>
+        /// <returns></returns>
+        SortedList<string, List<string>> GetProperties(NameValueCollection headers);
+    }
+}
