3fa9042e2895124f6b8b058e6d34f2b4d715079a
James Rosewell <james@51degrees.com>
2015-07-11 15:06:16 +0100

    Unit tests for interop wrappers

---
+++ VisualStudio/UnitTests/Performance/Enterprise/PatternAll.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FiftyOne.UnitTests.Performance.Enterprise
+{
+    [TestClass]
+    public class PatternAll : PatternBase
+    {
+        protected override int MaxInitializeTime
+        {
+            get { return 250; }
+        }
+
+        protected override string DataFile
+        {
+            get { return Constants.Enterprise_PATTERN_V32; }
+        }
+
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_InitializeTimeAll()
+        {
+            base.InitializeTime();
+        }
+        
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_BadUserAgentsMultiAll()
+        {
+            base.BadUserAgentsMulti(1);
+        }
+
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_BadUserAgentsSingleAll()
+        {
+            base.BadUserAgentsSingle(1);
+        }
+
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_UniqueUserAgentsMultiAll()
+        {
+            base.UniqueUserAgentsMulti(1);
+        }
+
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_UniqueUserAgentsSingleAll()
+        {
+            base.UniqueUserAgentsSingle(1);
+        }
+
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_RandomUserAgentsMultiAll()
+        {
+            base.RandomUserAgentsMulti(1);
+        }
+
+        [TestMethod]
+        public void EnterpriseV32Pattern_Performance_RandomUserAgentsSingleAll()
+        {
+            base.RandomUserAgentsSingle(1);
+        }
+    }
+}
---
+++ VisualStudio/UnitTests/Performance/PatternBase.cs
@@ -0,0 +1,40 @@
+﻿/* *********************************************************************
+ * This Source Code Form is copyright of 51Degrees Mobile Experts Limited. 
+ * Copyright © 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
+ * 
+ * This Source Code Form is the subject of the following patent 
+ * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+ * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY: 
+ * European Patent Application No. 13192291.6; and
+ * United States Patent Application Nos. 14/085,223 and 14/085,301.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0.
+ * 
+ * If a copy of the MPL was not distributed with this file, You can obtain
+ * one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is “Incompatible With Secondary Licenses”, as
+ * defined by the Mozilla Public License, v. 2.0.
+ * ********************************************************************* */
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FiftyOne.Mobile.Detection.Provider.Interop;
+using System.Collections.Generic;
+
+namespace FiftyOne.UnitTests.Performance
+{
+    public abstract class PatternBase : Base
+    {
+        [TestInitialize]
+        public void Initialise()
+        {
+            var start = DateTime.UtcNow;
+            Utils.CheckFileExists(DataFile);
+            _wrapper = new PatternWrapper(DataFile, null, 5000);
+            _testInitializeTime = DateTime.UtcNow - start;
+        }
+    }
+}
