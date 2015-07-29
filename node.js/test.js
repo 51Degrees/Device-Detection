d791dcb73450c90de784b715bf91f0c0e18b8860
James Rosewell <james@51degrees.com>
2015-07-29 17:30:50 +0100

    Updated to provide available properties method

--- node.js/test.js
+++ node.js/test.js
@@ -1,23 +1,24 @@
 /* *********************************************************************
- * This Source Code Form is copyright of 51Degrees Mobile Experts Limited.
- * Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
- * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
- *
- * This Source Code Form is the subject of the following patent
- * applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
- * Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY:
- * European Patent Application No. 13192291.6; and
- * United States Patent Application Nos. 14/085,223 and 14/085,301.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0.
- *
- * If a copy of the MPL was not distributed with this file, You can obtain
- * one at http://mozilla.org/MPL/2.0/.
- *
- * This Source Code Form is "Incompatible With Secondary Licenses", as
- * defined by the Mozilla Public License, v. 2.0.
- ********************************************************************** */
+* This Source Code Form is copyright of 51Degrees Mobile Experts Limited.
+* Copyright 2014 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+* Caversham, Reading, Berkshire, United Kingdom RG4 7BY and Yorkshire, 
+* Inc. Copyright 2014 Yorkshire, Inc, Guiyang, Guizhou, China
+*
+* This Source Code Form is the subject of the following patent
+* applications, owned by 51Degrees Mobile Experts Limited of 5 Charlotte
+* Close, Caversham, Reading, Berkshire, United Kingdom RG4 7BY:
+* European Patent Application No. 13192291.6; and
+* United States Patent Application Nos. 14/085,223 and 14/085,301.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0.
+*
+* If a copy of the MPL was not distributed with this file, You can obtain
+* one at http://mozilla.org/MPL/2.0/.
+*
+* This Source Code Form is "Incompatible With Secondary Licenses", as
+* defined by the Mozilla Public License, v. 2.0.
+********************************************************************** */
 
 var test = require('tape');
 var crypto = require('crypto');
@@ -25,11 +26,10 @@ var fs = require('fs');
 var zlib = require('zlib');
 var path = require('path');
 var Parser = require('./index').Parser;
-var properties = require('./index').ALL_PROPERTIES;
 var configmock = require('httpmocker').config;
 
-var patternFileName = '../data/51Degrees-LiteV3.2.dat';
-var trieFileName = '../data/51Degrees-LiteV3.2.trie';
+var patternFileName = process.argv.length > 2 ? process.argv[2] : '../data/51Degrees-LiteV3.2.dat';
+var trieFileName = process.argv.length > 3 ? process.argv[3] : '../data/51Degrees-LiteV3.2.trie';
 var userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36';
 var ua_src = fs.readFileSync(path.join(__dirname, '../data/20000 User Agents.csv'));
 var ua_array = (ua_src + '').split('\n');
@@ -47,9 +47,9 @@ test('constructor arguments valid', function (t) {
 });
 
 test('pattern', function (t) {
-    var parser = new Parser(patternFileName, properties);
+    var parser = new Parser(patternFileName);
     var ret = parser.parse(userAgent);
-    properties.forEach(function (property) {
+    parser.availableProperties().forEach(function (property) {
         t.ok(typeof ret[property] !== undefined, property + '> ok');
     });
     t.equal(ret.method, 'pattern');
@@ -57,9 +57,9 @@ test('pattern', function (t) {
 });
 
 test('trie', function (t) {
-    var parser = new Parser(trieFileName, properties);
+    var parser = new Parser(trieFileName);
     var ret = parser.parse(userAgent);
-    properties.forEach(function (property) {
+    parser.availableProperties().forEach(function (property) {
         t.ok(typeof ret[property] !== undefined, property + '> ok');
     });
     t.equal(ret.method, 'trie');
@@ -71,7 +71,7 @@ test('pattern overflow', function (t) {
     for (var i = 0; i < 1000; i++) {
         ua[i] = 50;
     }
-    var parser = new Parser(patternFileName, properties);
+    var parser = new Parser(patternFileName);
     var throwed = false;
     try { parser.parse(ua); }
     catch (e) {
@@ -83,11 +83,11 @@ test('pattern overflow', function (t) {
 
 test('empty userAgent', function (t) {
     var r;
-    var psr1 = new Parser(patternFileName, properties);
+    var psr1 = new Parser(patternFileName);
     r = psr1.parse('');
     t.equal(r.method, 'pattern');
 
-    var psr2 = new Parser(trieFileName, properties);
+    var psr2 = new Parser(trieFileName);
     r = psr2.parse('');
     t.equal(r.method, 'trie');
     t.end();
@@ -96,11 +96,11 @@ test('empty userAgent', function (t) {
 test('random userAgent', function (t) {
     var r;
     var ua = crypto.pseudoRandomBytes(10);
-    var psr1 = new Parser(patternFileName, properties);
+    var psr1 = new Parser(patternFileName);
     r = psr1.parse(ua);
     t.equal(r.method, 'pattern');
 
-    var psr2 = new Parser(trieFileName, properties);
+    var psr2 = new Parser(trieFileName);
     r = psr2.parse(ua);
     t.equal(r.method, 'trie');
     t.end();
@@ -186,7 +186,7 @@ test('auto update', function (t) {
 
 test('memory leak at pattern', function (t) {
     var cp1 = process.memoryUsage().rss / 1024 / 1024;
-    var psr1 = new Parser(patternFileName, properties);
+    var psr1 = new Parser(patternFileName);
     ua_array.forEach(function (ua) {
         patternFileName
         psr1.parse(ua);
@@ -198,7 +198,7 @@ test('memory leak at pattern', function (t) {
 
 test('memory leak at trie', function (t) {
     var cp1 = process.memoryUsage().rss / 1024 / 1024;
-    var psr1 = new Parser(trieFileName, properties);
+    var psr1 = new Parser(trieFileName);
     ua_array.forEach(function (ua) {
         psr1.parse(ua);
     });
