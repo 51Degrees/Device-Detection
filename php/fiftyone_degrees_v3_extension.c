8b982936a5e26f4a363b78b68b0eae96335fcd07
nerru86 <nerru86@gmail.com>
2015-09-14 16:39:05 +0100

    Several changes. 1: Added new function that accepts a string containing multiple HTTP headers. Each line needs to be separated by a new line character and header name must be separated from value by either a space or a colon. 2: Added comments to all functions. 3: The number of Worksets in the pool can now be set in the PHP.ini file - fiftyone_degrees.number_worksets. 4: Dataset init check has been moved to a separate function to reduce code size.

--- php/fiftyone_degrees_v3_extension.c
+++ php/fiftyone_degrees_v3_extension.c
@@ -6,7 +6,7 @@
 #include "php.h"
 #include "../src/pattern/51Degrees.h"
 
-#define PHP_EXTENSION_VERSION "3.1"
+#define PHP_EXTENSION_VERSION "3.2"
 #define PHP_EXTENSION_EXTNAME "FiftyOne_Degrees_Detector"
 
 extern zend_module_entry fiftyone_degrees_detector_module_entry;
@@ -17,6 +17,8 @@ PHP_FUNCTION(fiftyone_degrees_test_function);
 PHP_FUNCTION(fiftyone_degrees_get_properties);
 PHP_FUNCTION(fiftyone_match);
 PHP_FUNCTION(fiftyone_info);
+PHP_FUNCTION(fiftyone_http_headers);
+PHP_FUNCTION(fiftyone_match_with_headers);
 
 /* Declaration of Init and Shutdown functions for module and request. */
 PHP_MINIT_FUNCTION(fiftyone_degrees_detector_init);
@@ -32,6 +34,8 @@ static zend_function_entry fiftyone_degrees_detector_functions[] = {
   PHP_FE(fiftyone_degrees_get_properties, NULL)
   PHP_FE(fiftyone_match, NULL)
   PHP_FE(fiftyone_info, NULL)
+  PHP_FE(fiftyone_http_headers, NULL)
+  PHP_FE(fiftyone_match_with_headers, NULL)
   {NULL, NULL, NULL}
 };
 
@@ -78,24 +82,14 @@ PHP_FUNCTION(fiftyone_degrees_test_function)
  * as the name is shorter and moe logical. Function takes in a
  * user agent string from $_SERVER['HTTP_USER_AGENT'] and returns
  * an array with device properties.
+ * @return a PHP array of property:value pairs.
  */
 PHP_FUNCTION(fiftyone_match) {
-    //Verify the dataset initialised successfully.
-    switch (initStatus) {
-    case DATA_SET_INIT_STATUS_INSUFFICIENT_MEMORY:
-        php_error(E_WARNING, "51Degrees data set could not be created as not enough memory could be allocated.");
+    //Stop if there was a problem initialising the dataset.
+    if (datasetInitSuccess() == 0)
+    {
+        php_printf("Dataset initialisation failed.");
         return;
-    case DATA_SET_INIT_STATUS_CORRUPT_DATA:
-        php_error(E_WARNING, "51Degrees data set could not be created as a corrupt data file has been provided. Check it is uncompressed.");
-        return;
-    case DATA_SET_INIT_STATUS_INCORRECT_VERSION:
-        php_error(E_WARNING, "51Degrees data set could not be created as the data file is an unsupported version. Check you have the latest version of the data and api.");
-        return;
-    case DATA_SET_INIT_STATUS_FILE_NOT_FOUND:
-        php_error(E_WARNING, "51Degrees data set could not be created as a data file could not be found.");
-        return;
-    case DATA_SET_INIT_STATUS_SUCCESS: // do nothing
-        break;
     }
 
     char* useragent;
@@ -152,10 +146,40 @@ PHP_FUNCTION(fiftyone_match) {
 }
 
 /**
- * Function generates a report with some useful information.
+ * Experimental function to get access to the HTTP headers directly from this
+ * module. Do not use.
+ */
+PHP_FUNCTION(fiftyone_http_headers)
+{
+
+    int entries = zend_llist_count(&SG(sapi_headers).headers);
+    php_printf("Zend header Entries: %d<br/>", entries);
+
+    zend_llist_position pos;
+    sapi_header_struct* h;
+
+    for (h = (sapi_header_struct*)zend_llist_get_first_ex(&SG(sapi_headers).headers, &pos);
+         h;
+         h = (sapi_header_struct*)zend_llist_get_next_ex(&SG(sapi_headers).headers, &pos))
+    {
+        php_printf("SAPI %.*s <br/>", h->header_len, h->header);
+    }
+}
+
+/**
+ * Function generates a report with some useful information about the data set.
+ * Information includes things like the number of device combinations, the
+ * published and the next update date for the data file.
  */
 PHP_FUNCTION(fiftyone_info)
 {
+    //Stop if there was a problem initialising the dataset.
+    if (datasetInitSuccess() == 0)
+    {
+        php_printf("Dataset initialisation failed.");
+        return;
+    }
+
     php_printf("<h3>Dataset</h3>");
     php_printf("<p>Init status %d</p>", initStatus);
     php_printf("<p>Published:\t\t%d/%d/%d\r\n</p>",
@@ -178,31 +202,89 @@ PHP_FUNCTION(fiftyone_info)
 }
 
 /**
- * Function creates a new workset on each request and does not care
- * for the existence of pool or cache. Deprecated and should not be
- * used.
+ * Function performs device matching based on the multiple HTTP user agents.
+ * @param headers: a string of HTTP header:value pairs where each line is
+ * separated by the new line character. The header name is separated from the
+ * header value either by space or by colon.
+ * @return a PHP array of property:value entries.
+ */
+PHP_FUNCTION(fiftyone_match_with_headers)
+{
+    //Stop if there was a problem initialising the dataset.
+    if (datasetInitSuccess() == 0)
+    {
+        return;
+    }
+
+    char *headers;
+    int headers_len;
+    if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &headers, &headers_len) == SUCCESS) {
+        //Initialise array for returned values.
+        array_init(return_value);
+        //Match the provided user agent.
+        fiftyoneDegreesMatchWithHeadersString(wSet, headers, strlen(headers));
+
+        int propertyIndex;
+        for(propertyIndex = 0; propertyIndex < wSet->dataSet->requiredPropertyCount; propertyIndex++) {
+            char* propertyName = fiftyoneDegreesGetPropertyName(wSet->dataSet, *(wSet->dataSet->requiredProperties + propertyIndex));
+            int valueCount = fiftyoneDegreesSetValues(wSet, propertyIndex);
+            if (valueCount == 1) {
+                char *valueString = fiftyoneDegreesGetValueName(wSet->dataSet, *wSet->values);
+                add_assoc_string(return_value, propertyName, valueString, 1);
+            } else if (valueCount > 1) {
+                zval* valueArray;
+                ALLOC_INIT_ZVAL(valueArray);
+                array_init(valueArray);
+                add_assoc_zval(return_value, propertyName, valueArray);
+                int valueIndex;
+                for(valueIndex = 0; valueIndex < valueCount; valueIndex++) {
+                    char *valueString = fiftyoneDegreesGetValueName(wSet->dataSet, *(wSet->values + valueIndex));
+                    add_next_index_string(valueArray, valueString, 1);
+                }
+            }
+        }
+
+        //Add signature rank information.
+        int32_t sigRank = getSignatureRank(wSet);
+        add_assoc_long(return_value, "SignatureRank", sigRank);
+        add_assoc_long(return_value, "Difference", wSet->difference);
+
+        //Add method that was used for detection to the results array.
+        char* methodString;
+        switch(wSet->method){
+            case NONE: methodString = "None"; break;
+            case EXACT: methodString = "Exact"; break;
+            case NUMERIC: methodString = "Numeric"; break;
+            case NEAREST: methodString = "Nearest"; break;
+            case CLOSEST: methodString = "Closest"; break;
+            default: methodString = "Unknown"; break;
+        }
+        add_assoc_string(return_value, "Method", methodString, 1);
+
+    }
+    else
+    {
+        php_error(E_WARNING, "Could not parse arguments.");
+    }
+}
+
+/**
+ * Function creates a new workset for each request and does not care for the
+ * existence of pool or cache. Deprecated and should not be used.
+ * @deprecated instead use fiftyone_match or
+ *             fiftyone_match_with_headers.
  */
 PHP_FUNCTION(fiftyone_degrees_get_properties)
 {
-    switch (initStatus) {
-    case DATA_SET_INIT_STATUS_INSUFFICIENT_MEMORY:
-          php_error(E_WARNING, "51Degrees data set could not be created as not enough memory could be allocated.");
-          return;
-    case DATA_SET_INIT_STATUS_CORRUPT_DATA:
-          php_error(E_WARNING, "51Degrees data set could not be created as a corrupt data file has been provided. Check it is uncompressed.");
-          return;
-    case DATA_SET_INIT_STATUS_INCORRECT_VERSION:
-          php_error(E_WARNING, "51Degrees data set could not be created as the data file is an unsupported version. Check you have the latest version of the data and api.");
-          return;
-    case DATA_SET_INIT_STATUS_FILE_NOT_FOUND:
-          php_error(E_WARNING, "51Degrees data set could not be created as a data file could not be found.");
-          return;
-    case DATA_SET_INIT_STATUS_SUCCESS: // do nothing
-        break;
+    //Stop if there was a problem initialising the dataset.
+    if (datasetInitSuccess() == 0)
+    {
+        php_printf("Dataset initialisation failed.");
+        return;
     }
 
-      char* useragent;
-      int useragent_len;
+    char* useragent;
+    int useragent_len;
 
     if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &useragent, &useragent_len) == SUCCESS) {
         //Create a new workset.
@@ -260,6 +342,7 @@ PHP_FUNCTION(fiftyone_degrees_get_properties)
 
 /**
  * Function returns the rank of the signature that was matched to user agent.
+ * @return integer representing signature rank.
  */
 int32_t getSignatureRank(fiftyoneDegreesWorkset *ws) {
 	int32_t rank;
@@ -272,6 +355,31 @@ int32_t getSignatureRank(fiftyoneDegreesWorkset *ws) {
 	return INT32_MAX;
 }
 
+/**
+ * Function examines the DataSet init status and returns 0 if there was a
+ * problem or 1 otherwise.
+ * @return 1 if dataset successfully initialised, 0 otherwise.
+ */
+int datasetInitSuccess()
+{
+    switch (initStatus) {
+    case DATA_SET_INIT_STATUS_INSUFFICIENT_MEMORY:
+          php_error(E_WARNING, "51Degrees data set could not be created as not enough memory could be allocated.");
+          return 0;
+    case DATA_SET_INIT_STATUS_CORRUPT_DATA:
+          php_error(E_WARNING, "51Degrees data set could not be created as a corrupt data file has been provided. Check it is uncompressed.");
+          return 0;
+    case DATA_SET_INIT_STATUS_INCORRECT_VERSION:
+          php_error(E_WARNING, "51Degrees data set could not be created as the data file is an unsupported version. Check you have the latest version of the data and api.");
+          return 0;
+    case DATA_SET_INIT_STATUS_FILE_NOT_FOUND:
+          php_error(E_WARNING, "51Degrees data set could not be created as a data file could not be found.");
+          return 0;
+    case DATA_SET_INIT_STATUS_SUCCESS: // do nothing
+        return 1;
+    }
+}
+
 /* PHP Zend init and shutdown functions */
 
 /**
@@ -279,6 +387,7 @@ int32_t getSignatureRank(fiftyoneDegreesWorkset *ws) {
  */
 PHP_INI_BEGIN()
 PHP_INI_ENTRY("fiftyone_degrees.data_file", "/usr/lib/php5/51Degrees-Lite.dat", PHP_INI_ALL, NULL)
+PHP_INI_ENTRY("fiftyone_degrees.number_worksets", "10", PHP_INI_ALL, NULL)
 PHP_INI_END()
 
 /**
@@ -293,12 +402,13 @@ PHP_MINIT_FUNCTION(fiftyone_degrees_detector_init)
 {
     REGISTER_INI_ENTRIES();
     char* dataFilePath = INI_STR("fiftyone_degrees.data_file");
+    int number_worksets = INI_INT("fiftyone_degrees.number_worksets");
     dataSet = (fiftyoneDegreesDataSet*)malloc(sizeof(fiftyoneDegreesDataSet));
     initStatus = fiftyoneDegreesInitWithPropertyString((char*)dataFilePath, dataSet, NULL);
     int cacheSize = 1;
-    int poolSize = 10;
+    //int poolSize = 10;
     cache = fiftyoneDegreesResultsetCacheCreate(dataSet, cacheSize);
-    pool = fiftyoneDegreesWorksetPoolCreate(dataSet, cache, poolSize);
+    pool = fiftyoneDegreesWorksetPoolCreate(dataSet, cache, number_worksets);
     return SUCCESS;
 }
 
