a33b111ddfeb98378227cd2dac3c8448c4a76e4b
Ben Shillito <ben@51degrees.com>
2015-11-27 15:51:39 +0000

    EXAMPLE: Added premium and enterprise tests.

--- VisualStudio/CS Examples/Examples Tests/MatchForDeviceId.cs
+++ VisualStudio/CS Examples/Examples Tests/MatchForDeviceId.cs
@@ -8,9 +8,19 @@ namespace Examples_Tests
     public class MatchForDeviceId
     {
         [TestMethod]
-        public void Examples_Match_For_Device_Id()
+        public void Examples_Match_For_Device_Id_Lite()
         {
             Program.Run(Constants.LITE_PATTERN_V32);
+        }
+        [TestMethod]
+        public void Examples_Match_For_Device_Id_Premium()
+        {
+            Program.Run(Constants.PREMIUM_PATTERN_V32);
+        }
+        [TestMethod]
+        public void Examples_Match_For_Device_Id_Enterprise()
+        {
+            Program.Run(Constants.ENTERPRISE_PATTERN_V32);
         }
     }
 }
