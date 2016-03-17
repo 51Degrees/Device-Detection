7f32e0cd4fb0e277ba7540feaccc59207c659633
Mike M <mike@51degrees.com>
2016-03-17 12:21:15 +0000

    EXAMPLE/REORG/MEDIUM: Updated the following examples to use the Provider and retrieve worksets from pool. Also improved documentation. - MatchMetrics

--- examples/MatchMetrics.c
+++ examples/MatchMetrics.c
@@ -23,60 +23,59 @@
 Match metrics example of using 51Degrees device detection.
 The example shows how to:
 <ol>
-<li>Set the various settings for 51Degrees detector
-<p><pre class="prettyprint lang-c">
-const char* fileName = argv[1];
-const char* properties = "IsMobile";
-</pre></p>
-<li>Instantiate the 51Degrees device detection provider with these
-properties
-<p><pre class="prettyprint lang-c">
-fiftyoneDegreesInitWithPropertyString(fileName, &dataSet, properties);
-</pre></p>
-<li>Create a workset with which to find a match
-<p><pre class="prettyprint lang-c">
-ws = fiftyoneDegreesWorksetCreate(&dataSet, NULL);
-</pre></p>
-<li>Produce a match for a single HTTP User-Agent
-<p><pre class="prettyprint lang-c">
-fiftyoneDegreesMatch(ws, userAgent);
-</pre></p>
-<li>Obtain device Id: consists of four components separated by a hyphen
-symbol: Hardware-Platform-Browser-IsCrawler where each Component
-represents an ID of the corresponding Profile.
-<p><pre class="prettyprint lang-c">
-int deviceIdSize = ws->dataSet->header.components.count * 10;
-char deviceId[deviceIdSize];
-fiftyoneDegreesGetDeviceId(ws, deviceId, deviceIdSize)
-</pre></p>
-<li>Obtain match method: provides information about the
-algorithm that was used to perform detection for a particular User-Agent.
-For more information on what each method means please see:
-<a href="https://51degrees.com/support/documentation/pattern">
-How device detection works</a>
-<p><pre class="prettyprint lang-c">ws->method</pre></p>
-<li>Obtain difference:  used when detection method is not Exact or None.
-This is an integer value and the larger the value the less confident the
-detector is in this result.
-<p><pre class="prettyprint lang-c">ws->difference</pre></p>
-<li>Obtain signature rank: an integer value that indicates how popular
-the device is. The lower the rank the more popular the signature.
-<p><pre class="prettyprint lang-c">fiftyoneDegreesGetSignatureRank()</pre></p>
-<li>Release the memory taken by the workset
-<p><pre class="prettyprint lang-c">
-fiftyoneDegreesWorksetFree(ws);
-</pre></p>
-<li>Finally release the memory taken by the dataset
-<p><pre class="prettyprint lang-c">
-fiftyoneDegreesDataSetFree(&dataSet);
-</pre></p>
+	<li>Specify name of the data file and properties the dataset should be 
+	initialised with.
+	<p><pre class="prettyprint lang-c">
+	const char* fileName = argv[1];
+	const char* properties = "IsMobile";
+	</pre></p>
+	<li>Instantiate the 51Degrees provider from the specified data file with 
+	the required properties, number of worksets in the pool and cache of the 
+	specific size.
+	<p><pre class="prettyprint lang-c">
+	fiftyoneDegreesInitProviderWithPropertyString(
+	fileName, &provider, properties, 4, 1000);
+	</pre></p>
+	<li>Retrieve a workset from the pool and use it for a single match.
+	<p><pre class="prettyprint lang-c">
+	fiftyoneDegreesWorkset *ws = NULL;
+	ws = fiftyoneDegreesProviderWorksetGet(&provider);
+	</pre></p>
+	<li>Obtain match method: provides information about the
+	algorithm that was used to perform detection for a particular User-Agent.
+	For more information on what each method means please see:
+	<a href="https://51degrees.com/support/documentation/pattern">
+	How device detection works</a>
+	<p><pre class="prettyprint lang-c">ws->method</pre></p>
+	<li>Obtain difference:  used when detection method is not Exact or None.
+	This is an integer value and the larger the value the less confident the
+	detector is in this result.
+	<p>
+	<pre class="prettyprint lang-c">
+	ws->difference</pre>
+	</p>
+	<li>Obtain signature rank: an integer value that indicates how popular
+	the device is. The lower the rank the more popular the signature.
+	<p><pre class="prettyprint lang-c">
+	fiftyoneDegreesGetSignatureRank();
+	</pre></p>
+	<li>Release the workset back into the pool of worksets to be reused in one 
+	of the next matches.
+	<p><pre class="prettyprint lang-c">
+	fiftyoneDegreesWorksetRelease(ws);
+	</pre></p>
+	<li>Finally release the memory taken by the provider.
+	<p><pre class="prettyprint lang-c">
+	fiftyoneDegreesProviderFree(&provider);
+	</pre></p>
 </ol>
-This example assumes you have compiled with 51Degrees.c
-and city.c. This will happen automatically if you are compiling
-as part of the Visual Studio solution. Additionally, when running,
-the location of a 51Degrees data file must be passed as a
-command line argument if you wish to use Premium or Enterprise
-data files.
+<p>
+	This example assumes you have compiled with 51Degrees.c and city.c.
+	This will happen automatically if you are compiling as part of the
+	Visual Studio solution. Additionally, when running the program, the
+	location of a 51Degrees data file must be passed as a command line
+	argument if you wish to use Premium or Enterprise data files.
+</p>
 </tutorial>
 */
 
@@ -93,44 +92,34 @@ data files.
 #include <string.h>
 #include "../src/pattern/51Degrees.h"
 
-fiftyoneDegreesDataSet dataSet;
+// Global settings and properties.
+static fiftyoneDegreesProvider provider;
 
+// Function declarations.
+static void reportDatasetInitStatus(
+	fiftyoneDegreesDataSetInitStatus status,
+	const char* fileName);
 void output_match_metrics(fiftyoneDegreesWorkset* ws);
-int run(fiftyoneDegreesDataSet* dataSet);
+void run(fiftyoneDegreesProvider* provider);
 
 int main(int argc, char* argv[]) {
 	const char* properties = "IsMobile";
 	const char* fileName = argc > 1 ? argv[1] : "../../../data/51Degrees-LiteV3.2.dat";
 
-	/**
-	* Initialises the device detection dataset with the above settings.
-	* This uses the Lite data file For more info
-	* see:
-	* <a href="https://51degrees.com/compare-data-options">compare data options
-	* </a>
-	*/
-	if (fileName != NULL) {
-		switch (fiftyoneDegreesInitWithPropertyString(fileName, &dataSet, properties)) {
-			case DATA_SET_INIT_STATUS_INSUFFICIENT_MEMORY:
-				printf("Insufficient memory to load '%s'.", fileName);
-				break;
-			case DATA_SET_INIT_STATUS_CORRUPT_DATA:
-				printf("Device data file '%s' is corrupted.", fileName);
-				break;
-			case DATA_SET_INIT_STATUS_INCORRECT_VERSION:
-				printf("Device data file '%s' is not correct version.", fileName);
-				break;
-			case DATA_SET_INIT_STATUS_FILE_NOT_FOUND:
-				printf("Device data file '%s' not found.", fileName);
-				break;
-			case DATA_SET_INIT_STATUS_NOT_SET:
-				printf("Device data file '%s' could not be loaded.", fileName);
-				break;
-			default:
-				run(&dataSet);
-				break;
-		}
+	// Create a pool of 4 worksets with a cache for 1000 items.
+	fiftyoneDegreesDataSetInitStatus status =
+		fiftyoneDegreesInitProviderWithPropertyString(
+		fileName, &provider, properties, 4, 1000);
+	if (status != DATA_SET_INIT_STATUS_SUCCESS) {
+		reportDatasetInitStatus(status, fileName);
+		fgetc(stdin);
+		return 1;
 	}
+
+	run(&provider);
+
+	// Free the pool, dataset and cache.
+	fiftyoneDegreesProviderFree(&provider);
 
 	// Wait for a character to be pressed.
 	fgetc(stdin);
@@ -143,7 +132,7 @@ int main(int argc, char* argv[]) {
 
 	return 0;
 }
-int run(fiftyoneDegreesDataSet* dataSet) {
+void run(fiftyoneDegreesProvider* provider) {
     fiftyoneDegreesWorkset *ws = NULL;
 
     // User-Agent string of an iPhone mobile device.
@@ -162,31 +151,38 @@ int run(fiftyoneDegreesDataSet* dataSet) {
 
     printf("Starting Getting Started Match Metrics Example\n");
 
-    // Creates a workset.
-    ws = fiftyoneDegreesWorksetCreate(dataSet, NULL);
+	// Get a workset from the pool to perform this match.
+	ws = fiftyoneDegreesProviderWorksetGet(provider);
 
     // Carries out a match with a mobile User-Agent.
     printf("\nUser-Agent: %s\n", mobileUserAgent);
     fiftyoneDegreesMatch(ws, mobileUserAgent);
     output_match_metrics(ws);
 
+	// Release workset after match complete and workset no longer required.
+	fiftyoneDegreesWorksetRelease(ws);
+
+	// Get a workset from the pool to perform this match.
+	ws = fiftyoneDegreesProviderWorksetGet(provider);
+
     // Carries out a match with a desktop User-Agent.
     printf("\nUser-Agent: %s\n", desktopUserAgent);
     fiftyoneDegreesMatch(ws, desktopUserAgent);
     output_match_metrics(ws);
+
+	// Release workset after match complete and workset no longer required.
+	fiftyoneDegreesWorksetRelease(ws);
+
+	// Get a workset from the pool to perform this match.
+	ws = fiftyoneDegreesProviderWorksetGet(provider);
 
     // Carries out a match with a MediaHub User-Agent.
     printf("\nUser-Agent: %s\n", mediaHubUserAgent);
     fiftyoneDegreesMatch(ws, mediaHubUserAgent);
     output_match_metrics(ws);
 
-    // Frees the workset.
-    fiftyoneDegreesWorksetFree(ws);
-
-    // Frees the dataset.
-    fiftyoneDegreesDataSetFree(dataSet);
-
-	return 0;
+	// Release workset after match complete and workset no longer required.
+	fiftyoneDegreesWorksetRelease(ws);
 }
 
 /**
@@ -207,5 +203,37 @@ void output_match_metrics(fiftyoneDegreesWorkset* ws) {
     printf("   Difference: %d\n", difference);
     printf("   Rank: %d\n", rank);
     return;
+}
+
+/**
+* Reports the status of the data file initialization.
+*/
+static void reportDatasetInitStatus(fiftyoneDegreesDataSetInitStatus status,
+	const char* fileName) {
+	switch (status) {
+	case DATA_SET_INIT_STATUS_INSUFFICIENT_MEMORY:
+		printf("Insufficient memory to load '%s'.", fileName);
+		break;
+	case DATA_SET_INIT_STATUS_CORRUPT_DATA:
+		printf("Device data file '%s' is corrupted.", fileName);
+		break;
+	case DATA_SET_INIT_STATUS_INCORRECT_VERSION:
+		printf("Device data file '%s' is not correct version.", fileName);
+		break;
+	case DATA_SET_INIT_STATUS_FILE_NOT_FOUND:
+		printf("Device data file '%s' not found.", fileName);
+		break;
+	case DATA_SET_INIT_STATUS_NULL_POINTER:
+		printf("Null pointer to the existing dataset or memory location.");
+		break;
+	case DATA_SET_INIT_STATUS_POINTER_OUT_OF_BOUNDS:
+		printf("Allocated continuous memory containing 51Degrees data file "
+			"appears to be smaller than expected. Most likely because the"
+			" data file was not fully loaded into the allocated memory.");
+		break;
+	default:
+		printf("Device data file '%s' could not be loaded.", fileName);
+		break;
+	}
 }
 // Snippet End
