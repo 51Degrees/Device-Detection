1143562ece5df88f669c25921c9f5cb72017717c
Mike M <mike@51degrees.com>
2016-03-30 11:09:02 +0100

    FEAT/MINOR: Implemented reload example that uses multiple threads. Implements similar logic to the C reload examples.
    First example initializes the provider. Then launches several background threads that run device detection on the 20000 User Agents file or any other file provided by user. To verify the integrity of reloading the data set a hash code is calculated for each match. Only values of the selected properties are used for calculating the hash.
    
    Dataset reload runs while threads are performing detection. When the threads complete execution of the payload functionality the reload data set no longer runs.
    
    Match must be disposed of at the end of each detection, otherwise detection breaks after about 20 iterations.

--- VisualStudio/Reload Data File/Program.cs
+++ VisualStudio/Reload Data File/Program.cs
@@ -11,52 +11,72 @@ namespace FiftyOne.Example.Illustration.CSharp.Reload_Data_File
 {
     class Program
     {
-        Provider provider;
+        static Provider provider;
+        string deviceDataFile;
+        string userAfentsFile;
+        string propertiesToUse;
+        // Indicates how many threads have finished executing.
+        static int threadsFinished = 0;
 
-        public void Run(string fileName)
+        public Program(string deviceDataFile, string userAfentsFile, string propertiesToUse)
+        {
+            this.deviceDataFile = deviceDataFile;
+            this.userAfentsFile = userAfentsFile;
+            this.propertiesToUse = propertiesToUse;
+        }
+
+        public void Run()
         {
             // Threads that run device detection in the background.
-            int backgroundThreads = 4;
-            string properties = "IsMobile";
+            int numberOfThreads = 4;
+            // Contains references to background threads.
             Thread[] threads;
 
             Console.WriteLine("Starting the Reload Data File Example.");
-            provider = new Provider(fileName, properties);
 
-            threads = new Thread[backgroundThreads];
-            for (int i = 0; i < backgroundThreads; i++)
+            provider = new Provider(deviceDataFile, propertiesToUse);
+            threads = new Thread[numberOfThreads];
+            for (int i = 0; i < numberOfThreads; i++)
             {
                 threads[i] = new Thread(new ThreadStart(threadPayload));
                 threads[i].Start();
             }
 
-            Console.WriteLine(".");
+            while (threadsFinished < numberOfThreads)
+            {
+                provider.reloadFromFile();
+                Console.WriteLine("Provider reloaded.");
+                Thread.Sleep(50);
+            }
 
-            for (int i = 0; i < backgroundThreads; i++)
+            for (int i = 0; i < numberOfThreads; i++)
             {
                 threads[i].Join();
             }
 
-            Console.WriteLine("Complete.");
+            // Release resources held by the provider.
+            provider.Dispose();
+            Console.WriteLine("Threads finished: "+threadsFinished);
+            Console.WriteLine("Program execution complete. Press Enter to exit.");
             Console.ReadKey();
         }
 
         public static void Main(string[] args)
         {
             string fileName = args.Length > 0 ? args[0] : "../../../../../../data/51Degrees-LiteV3.2.dat";
-            Program program = new Program();
-            program.Run(fileName);
+            string userAgents = args.Length > 1 ? args[1] : "../../../../../../data/20000 User Agents.csv";
+            string properties = args.Length > 2 ? args[2] : "IsMobile,BrowserName";
+            Program program = new Program(fileName, userAgents, properties);
+            program.Run();
         }
 
         public void threadPayload()
         {
-            
-            string path = "D:\\Workspace\\Device-Detection\\data\\20000 User Agents.csv";
             int hash = 0;
             int recordsProcessed = 0;
             Match match;
 
-            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (FileStream fs = File.Open(userAfentsFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             using (BufferedStream bs = new BufferedStream(fs))
             using (StreamReader sr = new StreamReader(bs))
             {
@@ -64,12 +84,23 @@ namespace FiftyOne.Example.Illustration.CSharp.Reload_Data_File
                 while ((line = sr.ReadLine()) != null)
                 {
                     match = provider.getMatch(line);
-                    hash ^= match.GetHashCode();
+                    hash ^= getHash(match);
                     match.Dispose();
                     recordsProcessed++;
                 }
             }
+            Interlocked.Increment(ref threadsFinished);
             Console.WriteLine("Thread complete with hash code: " + hash + " and records processed: " + recordsProcessed);
+        }
+
+        public static int getHash(Match match)
+        {
+            int hash = -1;
+            foreach(var property in provider.getAvailableProperties()) 
+            {
+                hash += match.getValue(property).GetHashCode();
+            }
+            return hash;
         }
     }
 }
