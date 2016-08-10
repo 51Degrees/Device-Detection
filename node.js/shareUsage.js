ca65bd2473c96f7f210e591341722a3f8f4db1dd
ben51degrees <ben@51degrees.com>
2016-08-10 12:37:56 +0100

    REORG: Removed xmlbuilder dependancy.
    The dependancy has been removed and the xml instead built up as a
    string. Doing this means the addition of a charEscape function which
    replaces <, > and " symbols so the xml does not become unreadable.
    
    Related Work Items: #1272

--- node.js/shareUsage.js
+++ node.js/shareUsage.js
@@ -1,4 +1,3 @@
-var xmlBuilder = require("xmlbuilder");
 var zlib = require("zlib");
 var http = require("http");
 
@@ -51,7 +50,7 @@ var isLocal = function(address) {
 var sendData = function(outputStream) {
     log.emit('debug', 'Sending usage data to ' + requestOptions.host);
     var request = http.request(requestOptions, function(response) {
-        switch (response.statusCode) {
+        switch (response.statusCode)    {
             case 200:// OK
                 // Ok response, do nothing
                 break;
@@ -102,13 +101,13 @@ var sendData = function(outputStream) {
     zlib.gzip(xmlString, function(err, result) {
         request.write(result);
         request.end();
+        log.emit('debug', 'Usage data sent successfully');
     });
-    
-    log.emit('debug', 'Usage data sent successfully');
 }
 
 // Adds the request to the queue to be processed.
 shareUsage.recordNewDevice = function(request) {
+
     if (stop === false) {
 
         // Usage sharing has not been stopped, so get the device information.
@@ -124,48 +123,60 @@ shareUsage.recordNewDevice = function(request) {
     }
 }
 
+// Replaces symbols that may be interpreted as part of the xml.
+var charEscape = function(str) {
+    return str.replace(/\</g,"&lt;")
+        .replace(/\>/g,"&gt;")
+        .replace(/\"/g, "&quot;");
+}
+
 // Records the information as XML data and converts to a string for storage.
 var getContent = function(request) {
-
     // Begin the XML.
-    var device = xmlBuilder.create('Device');
+    var device = '<Device>';
 
     // Add the sender information.
-    device.ele('DateSent', new Date().toISOString())
-    device.ele('Version', version)
-    device.ele('Product', product)
+    device += '<DateSent>' + new Date().toISOString() + '</DateSent>';
+    device += '<Version>' + version + '</Version>';
+    device += '<Product>' + product + '</Product>';
 
     // Get the remote address.
     var remoteAddress = request.connection.remoteAddress;
     // Are you local?
     if (isLocal(remoteAddress.toString()) === true) {
-        // Strip the leading part of the ip when setting.
-        device.ele('ClientIP', remoteAddress.replace(/^.*:/, '').toString());
+        device += '<ClientIP>' + remoteAddress.toString().replace(/^.*:/, '')
+            + '</ClientIP>';
     }
 
     // Add the local address, removing the leading part.
     var localAddress = request.connection.localAddress.replace(/^.*:/, '');
-    device.ele('ServerIP', localAddress);
+    device += '<ServerIP>' + localAddress + '</ServerIP>';
 
     // Add the headers that are useful.
     Object.keys(request.headers).forEach(function (header) {
         if (header === 'user-agent'
             || header === 'host'
             || header.indexOf('profile') !== -1) {
-            device.ele('Header', {'Name': header}, request.headers[header]);        
+            device += '<Header Name="' + header + '">'
+                + charEscape(request.headers[header])
+                + '</Header>';
         }
     })
 
     // End the XML.
-    device.end();
+    device += '</Device>';
     
-    // Convert the XML to a string.
-    return device.toString();    
+    return device;
 }
 
 // Module constructor. Sets the error log, product version and product name.
 module.exports = function(provider, FOD) {
     log = FOD.log;
+    if (provider.config.UsageSharingDebug === true) {
+        requestOptions.host = 'localhost';
+        requestOptions.port = 1234;
+        newDeviceQueueLength = 1;
+    }
     version = provider.getDataSetFormat();
     product = 'Node js : ' + provider.config.Type;
     log.emit('info', 'Usage sharer started')
