a1bfa52d972e6e485175989a95c9b2809ef9a8f5
Ben Shillito <ben@51degrees.com>
2016-09-19 16:28:40 +0000

    BUG: Missing preinstall file.
    Preinstall file neccessary for successful build is now included in the core directory. This is an addition to commit e36d283.

---
+++ node.js/fiftyonedegreescore/gccCheck.js
@@ -0,0 +1,17 @@
+var exec = require('child_process').exec;
+var os = require('os');
+if (os.type() === "Linux")
+{
+	exec('gcc -dumpversion', function callback(error, stdout, stderr) {
+		var version = stdout.replace('\n','');
+		if (version < 4.7) {
+			throw "gcc is version " + version + " but must be 4.7 or greater";
+		}
+	});
+	exec('g++ -dumpversion', function callback(error, stdout, stderr) {
+		var version = stdout.replace('\n','');
+		if (version < 4.7) {
+			throw "g++ is version " + version + " but must be 4.7 or greater.";
+		}
+	});
+}
