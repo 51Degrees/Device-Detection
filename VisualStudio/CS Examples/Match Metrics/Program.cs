b62dd1e4fa25da4c38668f088c90001a913558a1
51DEGREES\joseph <joseph@51degrees.com>
2017-03-01 15:23:56 +0000

    LICENSE: Updated licenses in source files.

--- VisualStudio/CS Examples/Match Metrics/Program.cs
+++ VisualStudio/CS Examples/Match Metrics/Program.cs
@@ -1,6 +1,6 @@
 ﻿/**
  * This Source Code Form is copyright of 51Degrees Mobile Experts Limited.
- * Copyright (c) 2015 51Degrees Mobile Experts Limited, 5 Charlotte Close,
+ * Copyright (c) 2017 51Degrees Mobile Experts Limited, 5 Charlotte Close,
  * Caversham, Reading, Berkshire, United Kingdom RG4 7BY
  * 
  * This Source Code Form is the subject of the following patent
