5f8ecff145c48378f0e6c5543353725903406602
Ben Shillito <ben@51degrees.com>
2017-03-08 14:37:44 +0000

    OPTIM: The get max property value length function is now set at startup and makes no assumptions. All profiles are looked at to determine what the longest values string can be. This takes into account profiles where multiple values are returned and includes separator characters in the string length. This means that the nginx module no longer assumes what the longest list of properties can be, it allocated that exact maximum string length for each property.

--- src/pattern/51Degrees.h
+++ src/pattern/51Degrees.h
@@ -389,6 +389,7 @@ typedef struct fiftyoneDegrees_dataset_t {
 	fiftyoneDegreesHttpHeader *httpHeaders; /* Array of HTTP headers the data set can process */
 	const char **prefixedUpperHttpHeaders; /* Array of HTTP header strings in upper case form prefixed with HTTP_ */
 	fiftyoneDegreesProfilesStructArray *valuePointersArray;
+	int32_t *maxPropertyValueLength;
 	const char *fileName; /* Path to the data file used to create the data set */
 } fiftyoneDegreesDataSet;
 #pragma pack(pop)
@@ -1174,10 +1175,10 @@ EXTERNAL size_t fiftyoneDegreesGetProviderSizeWithPropertyCount(
 * property name.
 * @param dataSet pointer to a fiftyoneDegreesDataSet.
 * @param propertyName the name of the property to find the value length for.
-* @returns size_t the maximum string length of the values associated with the
-* given property.
+* @returns int32_t the maximum string length of the values associated with the
+* given property or -1 if the property was not found.
 */
-EXTERNAL size_t fiftyoneDegreesGetMaxValueLength(const fiftyoneDegreesDataSet *dataSet, char *propertyName);
+EXTERNAL int32_t fiftyoneDegreesGetMaxPropertyValueLength(const fiftyoneDegreesDataSet *dataSet, char *propertyName);
 
 /**
  * OBSOLETE METHODS - RETAINED FOR BACKWARDS COMPAITABILITY
