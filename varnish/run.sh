874fb3057285e02f03ec003084cfaa16c817eb9a
Ben51Degrees <ben@51degrees.com>
2017-04-26 14:45:03 +0100

    CLEANUP: Cleaned up some varnish files which were causing problems.
    Makefile.am now uses the markdown readme file and the licence from the
    parent directory. The example VCL now uses a Lite data file which is now
    coppied when running run.sh for the first time. The licence headers on
    all examples have been changed to be commented out correctly, and the
    version test changed to reflect the naming of the method.

--- varnish/run.sh
+++ varnish/run.sh
@@ -1,3 +1,8 @@
 sudo killall varnishd
+
+if [ ! -e "/etc/varnish/51Degrees.dat" ]; then
+	sudo mkdir /etc/varnish -p
+	sudo cp data/51Degrees-LiteV3.2.dat /etc/varnish/51Degrees.dat -v
+fi
 sudo /usr/local/sbin/varnishd -a localhost:99 -f example.vcl
 echo "Started varnish on port 99 with config 'example.vcl'."
