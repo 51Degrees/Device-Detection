544f0973166f7fe994b2234dd34c1e888e0c8a63
Ben51Degrees <ben@51degrees.com>
2017-09-13 13:53:52 +0100

    BUILD: Added a check for node version when installing. Versions 7 and above are not supported, so a message is displayed.

--- node.js/fiftyonedegreescore/versionCheck.js
+++ node.js/fiftyonedegreescore/versionCheck.js
@@ -2,6 +2,7 @@ var exec = require('child_process').exec;
 var os = require('os');
 if (os.type() === "Linux")
 {
+	// Check the GCC version.
 	exec('gcc -dumpversion', function callback(error, stdout, stderr) {
 		var version = stdout.replace('\n','');
 		if (version < 4.7) {
@@ -14,4 +15,12 @@ if (os.type() === "Linux")
 			throw "g++ is version " + version + " but must be 4.7 or greater.";
 		}
 	});
+}
+
+// Check the node version.
+var nodeVersion = process.versions.node;
+var nodeVersionMajor = nodeVersion.split(".")[0];
+console.log(nodeVersionMajor);
+if (nodeVersionMajor >= 7) {
+	throw "Node version must be < v7. For a beta version that supports 7 and up, use 'npm install fiftyonedegreescore@dev'";
 }
