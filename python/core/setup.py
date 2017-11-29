18989fb27b64fbff1ec336b04e9cf3c2bcb94400
51DEGREES\joseph <joseph@51degrees.com>
2017-11-29 16:20:50 +0000

    BUILD: Updated version numbers.

--- python/core/setup.py
+++ python/core/setup.py
@@ -26,7 +26,7 @@ if sys.version_info[0] == 3:
 
 setup(
     name='51degrees-mobile-detector',
-    version='3.2.15.2',
+    version='3.2.16.5',
     author='51Degrees',
     author_email='info@51degrees.com',
     packages=find_packages(),
