2f8fcce2f8cac148adf5fea8a4631bc42900680f
Paris Holley <mail@parisholley.com>
2019-10-27 17:20:25 -0400

    have to move files to project root because of rust-lang/cargo#6188

--- rust/build.rs
+++ rust/build.rs
@@ -3,8 +3,8 @@ fn main() {
         cc::Build::new()
             .define("FIFTYONEDEGREES_NO_THREADING", None)
             .warnings(false)
-            .file("../src/cityhash/city.c")
-            .file("../src/trie/51Degrees.c")
+            .file("src/cityhash/city.c")
+            .file("src/trie/51Degrees.c")
             .compile("trie");
     }
 
@@ -12,8 +12,8 @@ fn main() {
         cc::Build::new()
             .define("FIFTYONEDEGREES_NO_THREADING", None)
             .warnings(false)
-            .file("../src/cityhash/city.c")
-            .file("../src/pattern/51Degrees.c")
+            .file("src/cityhash/city.c")
+            .file("src/pattern/51Degrees.c")
             .compile("pattern");
     }
 }
